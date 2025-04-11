1a9ffb3e04d33eb3d7d267b4c0b402dcd270df08
Rostyslav Skliaruk <sklyaruk.rostislav1903@gmail.com>
2025-04-11 03:36:49 +0200

    homework 22

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,11 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+
         Car car = new Car(20, 7.5, LocalDate.of(2025, 5, 10));
         System.out.println("Машина може їхати? " + car.canDrive());
         System.out.println("Машина допущена до експлуатації? " + car.isAllowedToDrive());
         System.out.println("Максимальна дистанція: " + car.maxDistance() + "КМ");
     }
-
 
 }
